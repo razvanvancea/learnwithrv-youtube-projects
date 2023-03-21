8d618f8434a466a5a1cccc90d4e138ddab7fc58b
RV <razvanvancea94@gmail.com>
2023-03-21 10:14:25 +0200

    Update demo.js

--- pactumjs-mochawesome/demo.js
+++ pactumjs-mochawesome/demo.js
@@ -1,6 +1,22 @@
 const { spec } = require('pactum');
 
-it('should get random users', async () => {
+describe('Users test suite', ()=>{
+
+  
+it('should get random users1', async () => {
   await spec()
     .get('https://randomuser.me/api');
 });
+
+
+it('should get random users2', async () => {
+  await spec()
+    .get('https://randomuser.me/api');
+});
+
+
+it('should get random users- fails', async () => {
+  await spec()
+    .get('https://randomuser.me/api').expectStatus(500);
+});
+})
